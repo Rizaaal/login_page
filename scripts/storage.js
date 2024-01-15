41a6c0ef7dfd147f62ee8fd1e4769de903fd5160
Gabriele Di Grazia <gabrii539@gmail.com>
2024-01-15 18:57:46 +0100

    feat: add logUser function on storage.js

--- scripts/storage.js
+++ scripts/storage.js
@@ -3,3 +3,10 @@ export function getUserObject(mail){
     .find(user => user.mail === mail);
 }
 
+export function logUser(mail){
+    localStorage.setItem("logged", mail);
+    let loginCount = JSON.parse(localStorage.getItem("users"))
+    .find(user => user.mail === mail);
+    console.log(loginCount);
+}
+
