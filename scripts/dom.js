7270b23f149980f876f5ce6985ca4c6074ce8590
Gabriele Di Grazia <gabrii539@gmail.com>
2024-01-15 18:50:25 +0100

    feat: add showPage() function to dom.js

--- scripts/dom.js
+++ scripts/dom.js
@@ -11,15 +11,26 @@ function showLogin(){
     document.querySelector('button#login')
     .addEventListener('click', showPage);
 }
+function showPage(){
+    //su pagina di benvenuto non esiste input.value
+    //e logged === null solo su login page
+    if (localStorage.getItem('logged') === null){
+        logUser(document.querySelector('input').value);
+    };
+
+    addUser(localStorage.getItem("logged"));
+    //const loggedUser = getUserObject("barbapapa");
+    //console.log(!loggedUser);
+
+    document.body.innerHTML = 
+    `
 <nav>
-<ul>
-    <li><button id="back"><</button></li>
-</ul>
+        <li><button id="logout">logout</button></li>
 </nav>
-`;
+        
+    <h1>Welcome, ${localStorage.getItem("logged")}</h1>
+    `;
 
-
-loginButton.addEventListener('click', () => {
-    //const mainPage = document.body.innerHTML;
-    document.body.innerHTML = welcome;
-});
+    document.querySelector('button#logout')
+    .addEventListener('click', showLogin);
+}
